49307b5d71bb9b47802faf7a107dab4c142dbd21
Krishna_Priya_Nynaru <141242083+krishnapriya-nynaru@users.noreply.github.com>
2025-06-12 15:43:30 +0530

    Update install_ollama.sh

--- Ollama_openwebui/scripts/install_ollama.sh
+++ Ollama_openwebui/scripts/install_ollama.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+set -e
+if ! command -v curl &> /dev/null; then
+  echo "⚠️ curl not found. Installing..."
+  sudo apt update -y
+  sudo apt install -y curl
+else
+  echo "✅ curl is already installed."
+fi
+curl -fsSL https://ollama.com/install.sh | sh
+ollama serve &
+sleep 2
+ollama --version
+echo "✅ Ollama is installed and running."
